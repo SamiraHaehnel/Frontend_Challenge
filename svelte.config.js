72fb4a6bc4a6887576218ff805c6c1d554fdfb54
Samira Hähnel <saha@ausbildung.int>
2025-10-30 12:35:32 +0100

    fix: sync lockfile after adding netlify adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,17 +1,14 @@
-import adapter from "@sveltejs/adapter-auto";
 import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
+import adapterAuto from "@sveltejs/adapter-auto";
+import adapterNetlify from "@sveltejs/adapter-netlify";
+
+const isNetlify = process.env.NETLIFY === "true";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://svelte.dev/docs/kit/integrations
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-    // adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://svelte.dev/docs/kit/adapters for more information about adapters.
-    adapter: adapter(),
+    adapter: isNetlify ? adapterNetlify() : adapterAuto(),
   },
 };
 
